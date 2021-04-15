a11a07442fdd92bea6ff7d7346dcf4d67e05c39b
james <jodm522@gmail.com>
2021-04-15 18:34:19 -0500

    restructured

--- sql-orm-recipe-box-master/migrations/20210414231016-create-instructions.js
+++ sql-orm-recipe-box-master/migrations/20210414231016-create-instructions.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Instructions', {
+    return queryInterface.createTable("Instructions", {
       id: {
-        allowNull: false,
+        allowNull: true,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       specification: {
         allowNull: false,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       listOrder: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       recipeId: {
         allowNull: false,
         references: { model: "Recipes" },
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Instructions');
-  }
+    return queryInterface.dropTable("Instructions");
+  },
 };
